f495b5579a8c15d5b38175d2c6b5627439d4d00e
Brett Cave <brett@jemstep.com>
2017-02-08 19:47:28 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ RailsWeddingRsvp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => ENV[] }
+  config.action_mailer.default_url_options = { :host => ENV["APP_DOMAIN"] }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
