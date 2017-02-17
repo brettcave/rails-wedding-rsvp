de5782836702c2fb0b3bcfee29fba8148d8e34f4
Brett Cave <brett@jemstep.com>
2017-02-17 16:09:32 +0200

    fix config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 RailsWeddingRsvp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.eager_load = true
+
+  config.log_level = :info
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -9,7 +13,7 @@ RailsWeddingRsvp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
